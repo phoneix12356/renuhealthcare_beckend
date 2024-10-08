3ca8558417346ea7fce360c287dee9e7d9468885
Kartik <kartikkc397@outlook.com>
2024-10-08 21:12:09 +0530

    Added: Route to add and fetch question completed

--- server/routes/questions.route.js
+++ server/routes/questions.route.js
@@ -1,16 +1,26 @@
-import question from "../models/Question.model";
+// import question from "../models/Question.model";
+import Question from "../models/Question.model.js";
 import express from "express";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const qst = await question.find();
-  console.log(qst);
+  try {
+    const questions = await Question.find();
+    res.status(200).json(questions);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ error: err.message });
+  }
 });
 
 router.post("/", async (req, res) => {
   try {
-    const question = await question.create(req.body);
+    const questions = await Question.insertMany(req.body);
+    res.status(201).json({ Status: "Question added Succesfully!", Note: "Use the GET method on same route to see the questions" });
   } catch (err) {
     console.log(err.message);
+    res.status(500).json({ error: err.message });
   }
 });
+
+export default router;
