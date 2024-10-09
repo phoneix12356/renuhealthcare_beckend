9b7d6c6e64451fad60dbcb06accf50cbe425749c
phoneix12356 <95468972+phoneix12356@users.noreply.github.com>
2024-10-09 12:42:27 +0530

    merging

--- server/routes/questions.route.js
+++ server/routes/questions.route.js
@@ -4,18 +4,8 @@ import { getQuestions, addQuestions } from "../controllers/questionsController.j
 
 const router = express.Router();
 
-<<<<<<< HEAD
 
 router.get("/", getQuestions);
 router.post("/", addQuestions);
 
 export default router;
-=======
-router.get("/", async (req, res) => {
-   console.log('hello');
-});
-
-router.post("/", async (req, res) => {
-  console.log('hello');
-});
->>>>>>> main
