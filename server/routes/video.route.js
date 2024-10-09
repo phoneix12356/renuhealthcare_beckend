233fe7d3903f198bb73b62eef09b7929ff481428
Ruhit Baidya <ruhitbaidya01@gmail.com>
2024-10-09 19:46:54 +0600

    finish this work

--- server/routes/video.route.js
+++ server/routes/video.route.js
@@ -4,9 +4,9 @@ import * as videoRoutes from "../controllers/video.Controler.js";
 const videoRouter = express.Router();
 
 // Define unique routes for each action
-videoRouter.get('/videoGet', videoRoutes.getVideo); // Assuming this fetches video details
-videoRouter.post('/videoPost', videoRoutes.videoUpload); // Upload a video
-videoRouter.patch('/updateWatchTime', videoRoutes.updateContentWatchTime); // Update watch time (change to PATCH if applicable)
-videoRouter.delete('/deleteVideo', videoRoutes.deleteVideos); // Delete a video (change to DELETE if applicable)
+videoRouter.get('/videoGet', videoRoutes.getVideo); 
+videoRouter.post('/videoPost', videoRoutes.videoUpload); 
+videoRouter.patch('/updateWatchTime', videoRoutes.updateContentWatchTime); 
+videoRouter.delete('/deleteVideo', videoRoutes.deleteVideos); 
 
 export default videoRouter;
