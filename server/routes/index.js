f797e24fa315ecd2f76763415e7ecd8d9f1425da
Prashant2002pd <92350732+Prashant2002pd@users.noreply.github.com>
2024-10-09 19:06:06 +0530

    added route for course

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 import express from "express";
-import userRouter from './userRoutes.js';
-import testRouter from './test.route.js'
+import userRouter from "./userRoutes.js";
+import testRouter from "./test.route.js";
 import questions from "./questions.route.js";
+import courserouter from "./Course.route.js";
 
 const router = express.Router();
-router.use('/user', userRouter);
-router.use('/questions', questions);
-router.use('/test', testRouter);
+router.use("/user", userRouter);
+router.use("/questions", questions);
+router.use("/test", testRouter);
+router.use("/course", courserouter);
 
 export default router;
